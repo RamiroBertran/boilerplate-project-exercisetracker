579030eb02d1deb85a9a6cdf26259ced014ca51d
RamiroBertran <ramirobertra@gmail.com>
2023-06-29 15:38:04 -0300

    feat: add require modules

--- index.js
+++ index.js
@@ -1,18 +1,51 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-require('dotenv').config()
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mongoose = require("mongoose");
+const mongoURL = process.env.MONGO_URI;
+const url = require("url");
+const dns = require("dns");
+const bodyParser = require("body-parser");
+const addOneUsername = require("./playground-1.mongodb");
 
-app.use(cors())
-app.use(express.static('public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/views/index.html')
+app.use(cors());
+const usernameSchema = mongoose.Schema({
+  username: String,
+});
+const addExcersiseSchema = mongoose.Schema({
+  id: String,
+  description: String,
+  duration: String,
+  date: String,
 });
 
+const usernameModel = mongoose.Model("usernameModel", usernameSchema);
+const addExcersiseModel = mongoose.Model(
+  "addExcersiseModel",
+  addExcersiseSchema
+);
 
+mongoose
+  .connect(mongoURL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log("Connected to database");
+  })
+  .catch((error) => {
+    console.log(typeof mongoURL);
+    console.error("Error: " + error);
+  });
 
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.post("/api/users", (req, res) => {
+  let username = req.body.username;
+});
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
-  console.log('Your app is listening on port ' + listener.address().port)
-})
+  console.log("Your app is listening on port " + listener.address().port);
+});
